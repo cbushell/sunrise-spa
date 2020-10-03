caaf1e4f749191749979ff905583a37f53f41a04
Chris Bushell <c.bushell@gmail.com>
2020-10-03 18:30:05 +0100

    rename variables

--- netlify-functions/yotpo-create-order.js
+++ netlify-functions/yotpo-create-order.js
@@ -1,15 +1,15 @@
 import fetch from 'whatwg-fetch';
 
-const { CLIENT_ID } = process.env;
-const { CLIENT_SECRET } = process.env;
+const { YOTPO_CLIENT_ID } = process.env;
+const { YOTPO_CLIENT_SECRET } = process.env;
 
 const AUTH_API_ENDPOINT = 'https://api.yotpo.com/oauth/token';
 
 exports.handler = async () => fetch(AUTH_API_ENDPOINT, {
   method: 'POST',
   body: JSON.stringify({
-    client_id: CLIENT_ID,
-    client_secret: CLIENT_SECRET,
+    client_id: YOTPO_CLIENT_ID,
+    client_secret: YOTPO_CLIENT_SECRET,
     grant_type: 'client_credentials',
   }),
 })
