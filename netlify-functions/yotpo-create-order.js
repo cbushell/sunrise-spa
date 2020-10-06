6ba0a02ec112e946b065e48b46aa3387879b302a
Chris Bushell <c.bushell@gmail.com>
2020-10-06 20:42:52 +0100

    correct Yotpo auth params

--- netlify-functions/yotpo-create-order.js
+++ netlify-functions/yotpo-create-order.js
@@ -6,11 +6,9 @@ const { YOTPO_CLIENT_SECRET } = process.env;
 const AUTH_API_ENDPOINT = 'https://api.yotpo.com/oauth/token';
 
 exports.handler = async () => axios.post(AUTH_API_ENDPOINT, {
-  body: JSON.stringify({
-    client_id: YOTPO_CLIENT_ID,
-    client_secret: YOTPO_CLIENT_SECRET,
-    grant_type: 'client_credentials',
-  }),
+  client_id: YOTPO_CLIENT_ID,
+  client_secret: YOTPO_CLIENT_SECRET,
+  grant_type: 'client_credentials',
 })
   .then((response) => response.json())
   .then((json) => json)
