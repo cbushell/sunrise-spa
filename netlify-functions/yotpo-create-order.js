be62e402769b70df48fce853cf05eed8b460cc19
Chris Bushell <c.bushell@gmail.com>
2020-10-06 20:26:48 +0100

    replace whatwg-fetch with axios

--- netlify-functions/yotpo-create-order.js
+++ netlify-functions/yotpo-create-order.js
@@ -1,13 +1,11 @@
-require('whatwg-fetch');
-require('xmlhttprequest');
+const axios = import('axios');
 
 const { YOTPO_CLIENT_ID } = process.env;
 const { YOTPO_CLIENT_SECRET } = process.env;
 
 const AUTH_API_ENDPOINT = 'https://api.yotpo.com/oauth/token';
 
-exports.handler = async () => fetch(AUTH_API_ENDPOINT, {
-  method: 'POST',
+exports.handler = async () => axios.post(AUTH_API_ENDPOINT, {
   body: JSON.stringify({
     client_id: YOTPO_CLIENT_ID,
     client_secret: YOTPO_CLIENT_SECRET,
